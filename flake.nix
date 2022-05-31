3b72923fc737922c38510980a7969346d3f3c7b3
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-05-31 20:02:02 +0200

    still WIP on the model

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
 
     in rec {
       devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [ pyenv hdf5 ont_vbz_compression ];
+        buildInputs = with pkgs; [ pyenv hdf5 ont_vbz_compression nodejs ];
         HDF5_PLUGIN_PATH="${pkgs.hdf5}/lib:${pkgs.ont_vbz_compression}/lib";
       };
       #devShells."vbz" = pkgs.mkShell {
--- isosep.py
+++ isosep.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
-import h5py
 from collections import Counter
 from itertools import chain
-import pymc3 as mc
-import numpy as np
-from pymc3 import Model, Normal, HalfCauchy, sample
-from pymc3 import *
-import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
+from pymc3 import *
+from pymc3 import Model, Normal, HalfCauchy, sample
+import arviz as az
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pandas
+import pymc3 as mc
+
+# Jannes has extracted the IDs which correspond to 0%, 30%, 100% deuterium. However, for now we only
+# care about 0% vs 100% deuterium
+
+def getIdLabels(fname):
+  labels=set()
+  with open(fname, 'r') as r:
+    for line in r:
+      labels.add(line.strip())
+  return labels
 
 # Store both k=1 and k=2 statistics
 
@@ -29,16 +41,18 @@ def maximalRunOf(fast5):
 
 #
 
-def fast5Handler (fname):
+def fast5Handler (labels,fname):
   print(fname)
   fast5 = h5py.File(fname, 'r')
   ns = []
   ms = []
+  ls = []
   i=0
   for r in fast5.keys():
     i+=1
     if i>=100: break
     s = maximalRunOf(fast5[r])
+    rid = r.split('read_')[1]
     rawSignal = fast5[r]['Raw/Signal'][:]
     start = fast5[r][f'Analyses/Segmentation_{s:03d}/Summary/segmentation'].attrs['first_sample_template']
     fromStart = rawSignal[start:]
@@ -52,8 +66,33 @@ def fast5Handler (fname):
     nstats = NucleotideStats(fastq)
     ns.append(nstats)
     ms.append(np.mean(pA))
+    for l in labels:
+      if rid in l:
+        ls.append(l)
+        break
+    if rid in labels['0']:
+      ls.append(0)
+    elif rid in labels['100']:
+      ls.append(1)
+    elif rid in labels['30']:
+      ls.append(2)
+    else:
+      ls.append(-1)
   fast5.close()
-  return (ns,ms)
+  # TODO construct data frame, where we can then later cut things away
+  return pandas.DataFrame(
+    { 'nstats': ns
+    , 'means': ms
+    , 'labels': ls
+    })
+
+def createData(ks,pd):
+  xs = []
+  for n in pd['nstats']:
+    s1 = sum(n.k1.values())
+    arr = np.array([n.k1[k] / s1 for k in ks])
+    xs.append(arr)
+  return xs
 
 # TODO run a simple mono- and dinucleotide model for the mean of the signal. Do this for
 # raw data
@@ -62,37 +101,42 @@ def fast5Handler (fname):
 # beta * [A-count,C-count,G-count,T-count,1] ~ observed mean
 # now, we need to add the change due to deuterium, which might be a shift in beta?
 
-def model (ns, ms):
+def model (pdd):
+  pd=pandas.concat([pdd[pdd['labels']==0], pdd[pdd['labels']==1]])
+  print(len(pd))
   ks = set()
-  for n in ns:
+  for n in pd['nstats']:
     for k in n.k1.keys():
       ks.add(k)
   # create vectorized data
-  xs = []
-  for n in ns:
-    s1 = sum(n.k1.values())
-    arr = np.array([n.k1[k] / s1 for k in ks])
-    xs.append(arr)
+  xs = createData(ks,pd)
   with Model():
     XS = Data('X', value = np.vstack(xs))
-    ys = Data('y', value = np.array(ms))
+    ys = Data('y', value = np.array(pd['means']))
+    ls = Data('c', value = np.array(pd['labels']))
     intercept = Normal('Intercept', 0, sd=30)
-    beta = Normal('β', 0, sd=30, shape=(len(ks)))
-    mu = intercept + math.dot(XS, beta)
+    beta = Normal('β', 0, sd=30, shape=(len(ks), 1))
+    # additional change in beta, if the label equals 1
+    # gamma = Normal('γ', 0, sd=30, shape=(len(ks), 1))
+    mu = intercept + math.dot(XS, beta) # + ls * gamma)
     epsilon = HalfCauchy('ε', 5)
     likelihood = Normal('ys', mu, epsilon, observed = ys)
-    trace = sample(1000)
+    trace = sample(1000, chains=1, init="adapt_diag")
     traceDF = trace_to_dataframe(trace)
     print(traceDF.describe())
-    scatter_matrix(traceDF, figsize=(8,8))
-    plt.savefig('fuck.png')
+    #scatter_matrix(traceDF, figsize=(8,8))
+    traceplot(trace)
+    plt.savefig('fuck.pdf')
 
 def main ():
   print(f'PyMC3 v{mc.__version__}')
-  #dir = '/data/fass5/reads/heavy_atoms_deuterium_taubert/basecalled_fast5s/20220303_FAR96927_BC14-15-16_0-30-100_Deutrium_sequencing'
-  dir = '.'
-  ns, ms = fast5Handler(dir + '/' + 'FAR96927_a59606f5_0.fast5')
-  model(ns, ms)
+  labels={}
+  labels['0'] = getIdLabels('barcode14.ids')
+  labels['30'] = getIdLabels('barcode15.ids')
+  labels['100'] = getIdLabels('barcode16.ids')
+  dir = '/shared/choener/Workdata/heavy_atoms_deuterium_taubert/tests'
+  pd = fast5Handler(labels,dir + '/' + 'FAR96927_a59606f5_0.fast5')
+  model(pd)
 
 if __name__ == "__main__":
   main()
