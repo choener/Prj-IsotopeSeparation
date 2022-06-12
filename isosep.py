ea52a8548097e7c9d0863eafdd08b8c23ad72b40
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-06-12 18:50:48 +0200

    checking new model for isotope separation

--- isosep.py
+++ isosep.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
-from collections import Counter, OrderedDict
+from collections import Counter #, OrderedDict
 from itertools import chain
-from pandas.plotting import scatter_matrix
+#from pandas.plotting import scatter_matrix
 from pathlib import Path
-from pymc3 import *
-from pymc3 import Model, Normal, HalfCauchy, sample
+#from pymc import *
+from pymc import Model, Normal, HalfCauchy, sample, Dirichlet, HalfNormal
+import aesara.tensor as at
 import arviz as az
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pandas
-import pymc3 as mc
-import theano.tensor as tt
+import pymc as mc
+#import theano.tensor as tt
 from os.path import exists
 
 # Jannes has extracted the IDs which correspond to 0%, 30%, 100% deuterium. However, for now we only
@@ -30,8 +31,10 @@ def getIdLabels(fname):
 class NucleotideStats:
   def __init__(self, seq):
     s2 = [''.join(pair) for pair in zip(seq[:-1],seq[1:])]
+    s3 = [''.join(triplet) for triplet in zip(seq[:-2],seq[1:-1],seq[2:])]
     self.k1 = Counter(chain.from_iterable(seq))
     self.k2 = Counter(s2)
+    self.k3 = Counter(s3)
 
 #
 
@@ -43,6 +46,7 @@ def maximalRunOf(fast5):
   return max(rs)
 
 #
+# TODO Make this generic over the k-mers
 
 def fast5Handler (labels,fname):
   print(fname)
@@ -93,52 +97,49 @@ def fast5Handler (labels,fname):
 def relNucComp(pd):
   ks1 = set()
   ks2 = set()
+  ks3 = set()
   for n in pd['nstats']:
     for k in n.k1.keys():
       ks1.add(k)
     for k in n.k2.keys():
       ks2.add(k)
+    for k in n.k3.keys():
+      ks3.add(k)
   ks1=list(ks1)
   ks1.sort()
   ks2=list(ks2)
   ks2.sort()
-  #xsMat1, xsMat2 = createData(ks1,ks2,pd)
+  ks3=list(ks3)
+  ks3.sort()
+  print(ks1)
+  print(ks2)
+  print(ks3)
   xs1 = []
   xs2 = []
+  xs3 = []
   for n in pd['nstats']:
     s1 = sum(n.k1.values())
     s2 = sum(n.k2.values())
+    s3 = sum(n.k3.values())
     arr1 = np.array([n.k1[k] / s1 for k in ks1])
     arr2 = np.array([n.k2[k] / s2 for k in ks2])
+    arr3 = np.array([n.k3[k] / s3 for k in ks3])
     xs1.append(arr1)
     xs2.append(arr2)
+    xs3.append(arr3)
   pd['n1rel'] = xs1
   pd['n2rel'] = xs2
+  pd['n3rel'] = xs3
   xs1bs = np.vstack(pd['n1rel'][pd['labels']==0])
   xs1dt = np.vstack(pd['n1rel'][pd['labels']==1])
-#  print(xs1bs[:,0])
-#  print(xs1dt[:,0])
-#  az.plot_posterior({
-#    "A bs": xs1bs[:,0],
-#    "C bs": xs1bs[:,1],
-#    "G bs": xs1bs[:,2],
-#    "T bs": xs1bs[:,3],
-#  })
-##  plt.savefig('A.pdf')
-#  az.plot_posterior({
-#    "A dt": xs1dt[:,0],
-#    "C dt": xs1dt[:,1],
-#    "G dt": xs1dt[:,2],
-#    "T dt": xs1dt[:,3],
-#  })
-  fig, axes = plt.subplots(2,4, figsize=(40,10))
+  _, axes = plt.subplots(2,4, figsize=(40,10))
   for c,lbl in enumerate(ks1):
     ax = axes[0,c]
     az.plot_posterior({lbl + ' base': xs1bs[:,c]},ax=ax, rope=(0,1))
     ax = axes[1,c]
     az.plot_posterior({lbl + ' deut': xs1dt[:,c]},ax=ax, rope=(0,1))
   plt.savefig('nucleotide-distributions.pdf')
-  return # pd # np.asmatrix(xs1), np.asmatrix(xs2)
+  return
 
 # Histogram of the relative nucleotide compositions, divided by label. We want to make sure that we
 # don't accidentally condition on the nucleotide composition, instead of on the deuterium
@@ -155,24 +156,23 @@ def nucleotideHistogram (pd):
 # beta * [A-count,C-count,G-count,T-count,1] ~ observed mean
 # now, we need to add the change due to deuterium, which might be a shift in beta?
 
-def model (pdd):
+def modelDirichletNucs (pdd):
   pd=pandas.concat([pdd[pdd['labels']==0], pdd[pdd['labels']==1]])
   print(len(pd))
   with Model():
-    ys = Data('y', value = np.array(pd['means']))
-    ls = Data('c', value = np.array(pd['labels']))
+    ys = np.array(pd['means'])
+    ls = np.array(pd['labels'])
     #
-    xs1Mat = np.asmatrix(np.vstack(pd['n1rel'].values))
-    xs1 = Data('xs1', value = xs1Mat)
-    _, cols1 = xs1Mat.shape
+    xs1 = np.asmatrix(np.vstack(pd['n1rel'].values))
+    _, cols1 = xs1.shape
     print(cols1)
     baseDisp1  = Dirichlet('bs dsp 1', np.ones(cols1))
-    baseScale1 = Normal('bs scl 1', 0, sd=3)
+    baseScale1 = Normal('bs scl 1', 0, sigma=3)
     deutDisp1  = Dirichlet('dt dsp 1', np.ones(cols1))
-    deutScale1 = Normal('dt scl 1', 0, sd=3)
+    deutScale1 = Normal('dt scl 1', 0, sigma=3)
     mu = np.mean(pd['means'])
-    mu += baseScale1 * tt.dot(xs1,baseDisp1)
-    mu += deutScale1 * tt.dot(xs1,deutDisp1) * ls
+    mu += baseScale1 * at.dot(xs1,baseDisp1)
+    mu += deutScale1 * at.dot(xs1,deutDisp1) * ls
     #
     #xs2 = Data('xs2', value = xsMat2)
     #rows2, cols2 = xsMat2.shape
@@ -184,11 +184,11 @@ def model (pdd):
     #mu += deutScale2 * tt.dot(xs2,deutDisp2) * ls
     #
     epsilon = HalfCauchy('ε', 5)
-    likelihood = Normal('ys', mu, epsilon, observed = ys)
-    trace = sample(1000, chains=2, init="adapt_diag")
-    traceDF = trace_to_dataframe(trace)
-    print(traceDF.describe())
-    print(traceDF['bs scl 1'] < traceDF['dt scl 1'])
+    likelihood = Normal('ys', mu = mu, sigma = epsilon, observed = ys)
+    trace = sample(1000, return_inferencedata = True, init="adapt_diag")
+    #traceDF = trace_to_dataframe(trace)
+    #print(traceDF.describe())
+    #print(traceDF['bs scl 1'] < traceDF['dt scl 1'])
     #scatter_matrix(traceDF, figsize=(8,8))
     #traceplot(trace)
     az.plot_posterior(trace)
@@ -198,11 +198,63 @@ def model (pdd):
     # TODO extract the full trace so that I can run a prob that deutscale != 0
     # TODO extract statistics on nucleotide distribution, compare between classes to make sure we
     # don't accidentally train just on that
-    prob_diff = np.mean(trace[:]['bs scl 1'] < trace[:]['dt scl 1'])
-    print('P(mean_base < mean_deut) = %.2f%%' % (prob_diff * 100))
+    #prob_diff = np.mean(trace[:]['bs scl 1'] < trace[:]['dt scl 1'])
+    #print('P(mean_base < mean_deut) = %.2f%%' % (prob_diff * 100))
+
+# Normalizes all pandas data, return the new pd frame and the mean,sigma.
+
+def normalize(pdd):
+  pd = pandas.concat([pdd[pdd['labels']==0], pdd[pdd['labels']==1]])
+  mean = pd['means'].mean()
+  stddev = pd['means'].std()
+  pd['means'] = (pd['means'] - mean) / stddev
+  return pd, mean, stddev
+
+#
+#
+# NOTE a variant with 3-mers is possible but leads to divergence of the sampler somewhat often
+# TODO normalize everything; but over all data.
+
+def modelMixtureNucs(pdd):
+  pd = pandas.concat([pdd[pdd['labels']==0], pdd[pdd['labels']==1]])
+  pd, pdmean, pdstddev = normalize(pdd)
+  print(len(pd))
+  ys = np.array(pd['means'])
+  ls = np.array(pd['labels'])
+  xs1 = np.asmatrix(np.vstack(pd['n1rel'].values))
+  xs2 = np.asmatrix(np.vstack(pd['n2rel'].values))
+  xs3 = np.asmatrix(np.vstack(pd['n3rel'].values))
+  _, cols1 = xs1.shape
+  _, cols2 = xs2.shape
+  _, cols3 = xs3.shape
+  with Model() as model:
+    # should be at zero, since we normalized
+    mu = Normal('μ',np.mean(pd['means']))
+#    beta1 = Normal('β1', mu = np.zeros(cols1), sigma=10)
+    beta2 = Normal('β2', mu = np.zeros(cols2), sigma=10)
+#    beta3 = Normal('β3', mu = np.zeros(cols3), sigma=10)
+#    gamma = Dirichlet('γ', [0.5,0.5])
+    deut = HalfNormal('δ', sigma = 1)
+    ll = mu
+    ll += at.dot(xs2, beta2)
+#    ll += gamma[0] * at.dot(xs1, beta1)
+#    ll += gamma[1] * at.dot(xs2, beta2)
+    ll += deut * ls
+#    ll += gamma[2] * at.dot(xs3, beta3)
+    error = HalfCauchy('ε', beta = 10)
+    likelihood = Normal('ys', mu = ll, sigma = error, observed = ys)
+    trace = sample(3000, return_inferencedata = True)
+  az.plot_trace(trace)
+  plt.savefig('trace.pdf')
+  az.plot_posterior(trace, var_names=['ε', 'μ', 'δ', 'β2'])
+#  az.plot_posterior(trace, var_names='μ')
+#  az.plot_posterior(trace, var_names='γ')
+#  az.plot_posterior(trace, var_names='γ')
+  plt.savefig('posterior.pdf')
+
 
 def main ():
-  print(f'PyMC3 v{mc.__version__}')
+  print(f'PyMC v{mc.__version__}')
   labels={}
   labels['0'] = getIdLabels('barcode14.ids')
   labels['30'] = getIdLabels('barcode15.ids')
@@ -215,9 +267,12 @@ def main ():
     pdAll = pandas.read_pickle('./pdAll.pandas')
   else:
     pds = []
+    cnt = 0
     for path in Path(dir).rglob(f'*.fast5'):
       pd = fast5Handler(labels,path)
       pds.append(pd)
+      cnt += 1
+      #if cnt >= 10: break
     allpds = pandas.concat(pds)
     allpds.to_pickle('./pdAll.pandas')
     pdAll = allpds
@@ -225,7 +280,8 @@ def main ():
   nucleotideHistogram(pdAll)
   # only work with data that has known labels
   pd = pdAll[pdAll['labels'] != -1]
-  model(pd)
+  #modelDirichletNucs(pd)
+  modelMixtureNucs(pd)
 
 if __name__ == "__main__":
   main()
