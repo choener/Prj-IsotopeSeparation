c4bf9ffdb7c9de230f39e7145c99c45c99ba995f
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2023-12-13 23:02:29 +0100

    tiny script to prep data in parallel

---
+++ prepdata.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Prepare data for downstream analysis. Runs locally in parallel using the 'parallel' tool.
+
+go() {
+  echo $1
+  ./IsotopeSep/prepdata.py -i $1
+}
+
+export -f go
+parallel --jobs 6 go {} ::: ../_data/data/*
+
